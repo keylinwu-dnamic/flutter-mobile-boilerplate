53975b01c6c55be8b4d1f64dff97a096ed427b25
Jesús Espinoza <jespinozaf07@gmail.com>
2022-06-28 09:35:32 -0600

    WIP

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\DNAMIC\\flutter\\flutter_mobile_boilerplate" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=E:\\DNAMIC\\flutter\\flutter_mobile_boilerplate"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\DNAMIC\\flutter\\flutter_mobile_boilerplate\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\DNAMIC\\flutter\\flutter_mobile_boilerplate"
+  "FLUTTER_TARGET=E:\\DNAMIC\\flutter\\flutter_mobile_boilerplate\\lib\\flavors\\main_dev.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\DNAMIC\\flutter\\flutter_mobile_boilerplate\\.dart_tool\\package_config.json"
+)
